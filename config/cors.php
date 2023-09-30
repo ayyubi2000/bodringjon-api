e433293aeb767dccb58d1e8313726d9c0ab13d4d
ayyubi2000 <72436671+ayyubi2000@users.noreply.github.com>
2023-09-30 10:36:09 +0000

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', '*', 'sanctum/csrf-cookie', 'graphql'],
+
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
-    
-    'allowed_methods' => ['*'],
-    
-    'allowed_headers' => ['*'],
-    
-    'exposed_headers' => [],
-    
-    'max_age' => 0,
-    
-    'supports_credentials' => false,
 
-    'allowed_origins_patterns' => []
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 
 ];
