6fc9f31b4f45b51a4bbf195866c5ff8b815bb70d
ayyubi2000 <72436671+ayyubi2000@users.noreply.github.com>
2023-10-01 08:31:25 +0000

    asfsadf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Credentials: true');
 
 
 /*
