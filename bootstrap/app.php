7c83eceb0ed0dfcc14ff5195be4a164232f3fe92
ayyubi2000 <72436671+ayyubi2000@users.noreply.github.com>
2023-10-01 08:55:41 +0000

    asdf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,4 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Credentials: true');
-
-
 /*
 |--------------------------------------------------------------------------
 | Create The Application
